ff5dd43991f533f410f5e2dbb22ed8688f25eee8
pbaker <me@retrodict.com>
2011-01-05 00:46:18 +0530

    Got histogram working, need to create some grammers

--- stats.rb
+++ stats.rb
@@ -1,13 +1,14 @@
 require 'rubygems'
 require 'sinatra'
 require 'sqlite3'
+require 'time'
 
 #config constants
 DATABASE_FILE = 'wb_sms_water.sqlite'
 
 #HTML constants
 PREFIX = '<html>'
-BRAINS = '<style type="text/css">body{ background-color: #ddd; font-size: 3em; font-family: sans-serif; text-align: center; padding: 1em; } .special { background-color: #333; color: #fff; padding: .25em; } </style>' #.special:hover { text-decoration: underline; }
+BRAINS = '<style type="text/css">body{ background-color: #ddd; font-size: 2em; font-family: sans-serif; text-align: center; padding: 1em; } .special { background-color: #333; color: #fff; padding: .25em; font-weight: bold; font-size: 1.25em; } .notes { font-size: .5em; }</style>' #.special:hover { text-decoration: underline; }
 HEAD = "<head>\n\t<title>West Bengal SMS Water Reports: %s</title>\n #{BRAINS} </head>\n"
 BODY = '<body>%s</body>'
 SUFFIX = '</html>'
@@ -15,7 +16,8 @@ SUFFIX = '</html>'
 helpers do
   #short cut method to return first value from a (should be) select database query
   def q(query)
-    #sanitized_query = SQLite3::Database.quote(query) ?
+    #sanitized_query = SQLite3::Database.quote(query)
+    #puts sanitized_query
     @db.get_first_value(query)
   end
 end
@@ -26,9 +28,70 @@ end
 
 
 get '/' do
+  html_title = 'Overview'
+  
+  page_title = '<h1>Lumin Reports (West Bengal SMS Data)</h1>'
   query = q('select count(*) from wb_water_sms')
-  body = "There are <span class=\"special\">#{query}</span> reports to dig into!"
-  PREFIX + (HEAD % 'Overview') + (BODY % body) + SUFFIX
+  p1 = "<p>There are <span class=\"special\">#{query}</span> reports to dig into!</p>"
+  p2 = "<p class=\"notes\">For example, see this <a href=\"/histogram\">histogram</a>.</p>"
+  body = page_title + p1 + p2
+  
+  PREFIX + (HEAD % html_title) + (BODY % body) + SUFFIX
 end
 
+get '/histogram' do
+  html_title = 'Data histogram'
+  
+  page_title = '<h1>Histogram of SMS Water Data (West Bengal)</h1>'
+  
+  first_ts = Time.parse(q('select date from wb_water_sms order by date asc limit 1')).to_i
+  num_samples = q('select count(*) from wb_water_sms').to_i
+  last_ts = Time.parse(q('select date from wb_water_sms order by date desc limit 1')).to_i
 
+  #divide the total time that data has been collected into 10 buckets (alright 11 with the initial 0)
+  bucket_width = (last_ts - first_ts)/10 #approximation...
+
+  #find the number of revisions that have been added to the db during each of those buckets
+  counts = [0] #start with 0 at the beginning, running sum of collected samples
+  times = [first_ts]
+  (1..10).each do |i|
+    time = first_ts + bucket_width * i
+    times << time
+    date = Time.at(time).strftime('%Y-%m-%d')
+    query = "select count(*) from wb_water_sms where date <= '#{date}'"
+    puts query
+    #the running sum is the total that we had as of each time period
+    counts << q(query).to_i
+  end
+
+  url = "http://chart.apis.google.com/chart?" + 
+    "cht=lc" + #lxy" + 
+    "&chs=600x400" +
+    "&chd=t:" + counts.join(',') + #"&chd=t:0,10,20,40,80,90,95,99|0,20,30,40,50,60,70,80" + 
+    "&chdl=Water reports" + 
+    "&chxt=x,y" + 
+    "&chtt=" + "Samples over time".gsub(/\ /, '+') +
+    "&chxr=1,0," + num_samples.to_s +
+    "&chds=0," + num_samples.to_s +
+    "&chxl=0:|#{Time.at(times.first).strftime("%b %d %Y %H:%M")}|#{Time.at(times[5]).strftime("%b %d %Y")}|#{Time.at(times.last).strftime("%b %d %Y %H:%M")}" #"&chxl=0:|0|1|2|3|4|5|6|7|8|9|10"
+  image = "<img src=\"#{url}\" alt=\"A sweet google chart, that you're not seeting, unfortunately.\" title=\"Histogram\" />"
+
+  p1 = '<p class="notes">This is a histogram of all of the data.</p>'
+
+  #counts.join(' ') + '; ' + times.join(' ')
+  #Time.now.to_i.to_s + ' <br>' + 
+  #Time.at(first_ts).to_s + ' <br>' + 
+  #Time.at(last_ts).to_s + ' <br>' +
+  body = page_title + 
+    image + p1
+  
+  PREFIX + (HEAD % html_title) + (BODY % body) + SUFFIX
+end
+
+#html_title = ''
+#page_title = ''
+#
+#body = page_title + ...
+#
+#PREFIX + (HEAD % html_title) + (BODY % body) + SUFFIX
+#
